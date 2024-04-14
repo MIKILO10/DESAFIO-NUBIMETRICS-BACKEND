bfb92caecad80ea41657d7612244a75bdf2db655
Gonzalo Mamaní Lopez <gonzalomamani.ucse10@gmail.com>
2024-04-13 21:51:50 -0300

    Add:  Declaración de endpoint Paises

---
+++ DESAFIOS.NUBIMETRICS/DESAFIOS.NUBIMETRICS.Web.API/Controllers/PaisesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafios.Nubimetrics.Web.API.Controllers
+{
+    [Route("MyRestfulApp/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        [HttpGet("{pais:alpha}")]
+        public IActionResult GetByCountries(string pais)
+        {
+            return Ok(new string[] { pais });
+        }
+
+    }
+
+}
